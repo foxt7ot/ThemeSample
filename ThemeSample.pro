83b455f31176f18641e4a01e3fb0d67cd12f6366
MYaseenAKAKhan <myaseen.khan@hotmail.com>
2014-08-10 18:44:42 +0530

    Black And White Theme Explained
    Sample BlackBerry Cascade app to explain. How to incorporate Black and
    White background theme in an app

---
+++ ThemeSample.pro
@@ -0,0 +1,5 @@
+APP_NAME = ThemeSample
+
+CONFIG += qt warn_on cascades10
+
+include(config.pri)
---
+++ translations/ThemeSample.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../assets/main.qml" line="25"/>
+        <source>Theme Selection:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="28"/>
+        <source>Bright</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="33"/>
+        <source>Dark</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/main.qml" line="42"/>
+        <source>Hello Themes</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
